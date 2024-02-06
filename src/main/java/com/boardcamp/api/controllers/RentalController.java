90c5f71f55c7a9edd1c2f1d1f7979ca133f2ba90
Andrezza Souza <72293497+andrezzasouza@users.noreply.github.com>
2024-02-05 21:41:04 -0300

    ✨ feat: Implement POST /rentals with MVC layers and incomplete business logic

--- src/main/java/com/boardcamp/api/controllers/RentalController.java
+++ src/main/java/com/boardcamp/api/controllers/RentalController.java
@@ -1,16 +1,22 @@
 package com.boardcamp.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boardcamp.api.dtos.RentalDTO;
 import com.boardcamp.api.models.RentalModel;
 import com.boardcamp.api.services.RentalService;
+
+import jakarta.validation.Valid;
 
 @CrossOrigin
 @RestController
@@ -27,5 +33,16 @@ public class RentalController {
     List<RentalModel> rentals = rentalService.getAllRentals();
 
     return ResponseEntity.status(HttpStatus.OK).body(rentals);
+  }
+
+  @PostMapping
+  public ResponseEntity<Object> addNewRental(@RequestBody @Valid RentalDTO body) {
+    Optional<RentalModel> rental = rentalService.save(body);
+
+    if (!rental.isPresent()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário ou jogo não encontrado com os ids enviados.");
+    }
+
+    return ResponseEntity.status(HttpStatus.OK).body(rental.get());
   }
 }
--- src/main/java/com/boardcamp/api/services/RentalService.java
+++ src/main/java/com/boardcamp/api/services/RentalService.java
@@ -1,21 +1,47 @@
 package com.boardcamp.api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.boardcamp.api.dtos.RentalDTO;
+import com.boardcamp.api.models.CustomerModel;
+import com.boardcamp.api.models.GameModel;
 import com.boardcamp.api.models.RentalModel;
+import com.boardcamp.api.repositories.CustomerRepository;
+import com.boardcamp.api.repositories.GameRepository;
 import com.boardcamp.api.repositories.RentalRepository;
 
 @Service
 public class RentalService {
   final RentalRepository rentalRepository;
+  final GameRepository gameRepository;
+  final CustomerRepository customerRepository;
 
-  RentalService(RentalRepository rentalRepository) {
+  RentalService(
+      RentalRepository rentalRepository,
+      GameRepository gameRepository,
+      CustomerRepository customerRepository) {
     this.rentalRepository = rentalRepository;
+    this.gameRepository = gameRepository;
+    this.customerRepository = customerRepository;
   }
 
   public List<RentalModel> getAllRentals() {
     return rentalRepository.findAll();
+  }
+
+  public Optional<RentalModel> save(RentalDTO dto) {
+    Optional<GameModel> existingGame = gameRepository.findById((long) dto.getGameId());
+    Optional<CustomerModel> existingCustomer = customerRepository.findById((long) dto.getCustomerId());
+
+    if (!existingGame.isPresent() || !existingCustomer.isPresent()) {
+      return Optional.empty();
+    }
+
+    RentalModel newRental = new RentalModel(dto, existingCustomer.get(), existingGame.get());
+
+    return Optional.of(rentalRepository.save(newRental));
   }
 }
